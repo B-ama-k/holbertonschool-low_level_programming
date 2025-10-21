a8b3673bde90d4f02f028a3c91f8da32f6613ac6
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-21 07:29:55 +0000

    ex 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,6 @@
-gcc -o
-
 #include <stdio.h> // Include standard input/output library
 
 int main() {
-  printf("Programming is like building a multilingual puzzle\n"); // Print a message
+  puts("Programming is like building a multilingual puzzle\n"); // Print a message
   return 0; // Exit successfully
    }
