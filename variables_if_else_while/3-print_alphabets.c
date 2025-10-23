7143bbd511fc2ce29ff804d50a9dd3634a871244
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-23 10:01:54 +0000

    ex 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - entry point
+*
+* Return: always 0 (success)
+*/
+int main(void)
+{
+    char alPHA;
+    for (alPHA = 'a'; alPHA <= 'z'; alPHA++)
+putchar(alPHA);
+    for (alPHA = 'A'; alPHA <= 'Z'; alPHA++)
+putchar(alPHA);
+putchar('\n');
+return (0);
+}
