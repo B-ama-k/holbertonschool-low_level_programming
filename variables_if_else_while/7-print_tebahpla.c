dc7682fb93e38b06d3c7553cb2c47993772c85c1
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-23 22:48:00 +0000

    ex 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - entry point
+*
+* Return: always 0 (success)
+*/
+int main(void)
+{
+char tebahpla;
+for (tebahpla = 'z'; tebahpla <= 'a'; tebahpla--)
+putchar(tebahpla);
+putchar(tebahpla);
+return (0);
+}
