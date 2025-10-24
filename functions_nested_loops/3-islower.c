f3f605a0e7c7688cc84b22bebc3f6de0814dbd3e
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-24 20:48:38 +0000

    ex3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,13 +1,14 @@
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 /**
- * int _islower(int c); check si minuscule
+ * _islower - check si minuscule
  * @c: carac to check ASCII.
  * Return: always 1 if lower and 0 is not.
  */
 int _islower(int c)
 {
-if (c = 'a' && c <= 'z')
+if (c >= 'a' && c <= 'z')
 return (1);
 else
 return (0);
