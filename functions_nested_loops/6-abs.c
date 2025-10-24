41ea8cb8a7d575fa754d8226ac52b766d25d0ece
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-24 22:23:02 +0000

    ex 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - calcul value.
+ * @r: the absolut value.
+ * Return: the absolut value of r.
+ */
+int _abs(int r)
+{
+if (r < 0)
+return (-r);
+else
+return (r);
+}
