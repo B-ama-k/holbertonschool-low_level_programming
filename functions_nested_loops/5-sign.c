e706b5c406d5f51076d12ac9eb69eea93d3fd837
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-24 21:25:06 +0000

    ex 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _isalpha - check if lower & upper
+ * @c: char to check ASCII.
+ * Return: always 1 if lower and 0 is not.
+ */
+int print_sign(int n)
+{
+if (n >= '0')
+{
+return (1);
+printf('+');
+{
+if (n == '0')
+{
+return (0);
+printf('0');
+{
+else
+return (-1);
+printf ('-');
+}
+}
+}
+}
+}
