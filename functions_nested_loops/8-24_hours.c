aed173d44a2961838ca35c5d1bfa03d3c3d182b3
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-25 14:43:32 +0000

    ex 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,1 +1,24 @@
 #include "main.h"
+/**
+ * jack_bauer - clock
+ *
+ * Return: always 0 (success)
+ */
+void jack_bauer(void)
+{
+int hours;
+int minutes;
+
+for (hours = 0; hours < 24; hours++)
+{
+for (minutes = 0; minutes < 60; minutes++)
+{
+_putchar((hours / 10) + '0');
+_putchar((hours % 10) + '0');
+_putchar(':');
+_putchar((minutes / 10) + '0');
+_putchar((minutes % 10) + '0');
+_putchar('\n');
+}
+}
+}
