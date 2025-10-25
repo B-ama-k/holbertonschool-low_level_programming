d797c15cf331522251676de922b8ee59e6d69f2f
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-25 15:18:49 +0000

    ex 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - table de multiplications
+ *
+ * Return: always 0 (success)
+ */
+int add(int, int)
+
+{
+int a;
+int b;
+
+return (a + b);
+}
