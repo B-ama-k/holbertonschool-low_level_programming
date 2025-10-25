b6350feb99a4c3435a520a7cb3e0dec727d3cb2e
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-25 15:25:14 +0000

    ex 10

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,10 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * add(int, int) - a + b
- *
- * Return: always 0 (success)
- */
+* add - adds a + b
+* @a: number 1
+* @b: number 2
+* Return: result
+*/
 int add(int a, int b)
 {
 return (a + b);
