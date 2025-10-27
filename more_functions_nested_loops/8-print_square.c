ca57f824aaeb9b5aafa9a21cfec2458faf4f24d4
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-27 18:50:05 +0000

    ex 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_square - print a square
+* @size: size of the square
+*
+*/
+
+void print_square(int size)
+{
+int longueur, large;
+if (size > 0)
+{
+for (longueur = '0'; longueur < size; longueur++)
+{
+for (large = '0'; large < size; large++)
+_putchar('#');
+if (longueur == size - 1)
+continue;
+_putchar('\n');
+}
+}
+_putchar('\n')
+}
