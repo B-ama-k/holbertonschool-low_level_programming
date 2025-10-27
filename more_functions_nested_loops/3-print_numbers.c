70f1c53f4406cab8c156a122466ff85a95a80753
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-27 10:11:42 +0000

    ex 3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,13 +2,14 @@
 
 /**
 * print_numbers - print numbers.
-* @n: numbers to print.
+*
 * Return: (success)
 */
 
 void print_number(void)
 {
 int n;
+
 for (n = '0'; n <= '9'; n++)
 _putchar(n + '0');
 _putchar('\n');
