320cca130f5e750642fc05e51faf4cb18bb75e8f
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-27 13:02:14 +0000

    ex 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,13 +7,13 @@
 
 void more_numbers(void)
 {
-int (n);
-int (c);
-for (n = 0; n <= 10; n++)
+char (n);
+char (c);
+for (n = '0'; n <= 10; n++)
 {
-for (c = 0; c <= 15; c++)
+for (c = '0'; c <= 15; c++)
 {
-_putchar(c + '0');
+_putchar(c);
 }
 _putchar('\n');
 }
