f4673837c11e5392f05f0446ae15bc94d0563a16
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-27 14:27:09 +0000

    ex 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_diagonal - draws diagonal line
+* @n: number of line for char
+*
+*/
+
+void print_diagonal(int n)
+{
+int diag;
+int space;
+if (n <= 0)
+{
+for (diag = '0'; diag < n; diag++)
+for (space = '0'; space < diag; space++)
+_putchar('\\');
+_putchar(' ');
+}
+_putchar('\n');
+}
