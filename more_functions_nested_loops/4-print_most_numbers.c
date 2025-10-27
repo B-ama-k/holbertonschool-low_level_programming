734eccddf0c17779e2d667866b1a282c399b4b6a
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-27 10:34:59 +0000

    ex 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_most_numbers - print numbers.
+* @n: print all number but not 2 and 4.
+* Return: (success)
+*/
+
+void print_most_numbers(void)
+{
+int n;
+if (n >= '0' && n <= '9')
+{
+if (n != '2' && '4')
+_putchar(n + '0');
+_putchar('\n');
+}
+}
