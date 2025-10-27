25d46e45ce9d26ea30e507a6e252489c9d92056c
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-27 13:08:43 +0000

    ex 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,19 +1,21 @@
 #include "main.h"
 /**
-* more_numbers - print 10 digit from 0 to 14.
+* more_numbers - print 10 times 0 to 14.
 *
 *
 */
 
 void more_numbers(void)
 {
-char (n);
-char (c);
+int (n);
+int (c);
 for (n = '0'; n <= 10; n++)
 {
-for (c = '0'; c <= 15; c++)
+for (c = '0'; c <= 14; c++)
 {
-_putchar(c);
+if (c > 9)
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
 }
 _putchar('\n');
 }
