674c13df0a530f268bac5c9b3a64f2deb31e5306
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-28 13:12:41 +0000

    ex 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - entry
+*
+* return: 0 (success).
+*/
+int main(void)
+{
+int n;
+
+for (n = 1; n <= 100; n++)
+{
+if ((n % 3) == 0 && (n % 5) == 0)
+printf("fizzbuzz");
+else if ((n % 3) == 0)
+printf("fizz");
+else if ((n % 5) == 0)
+printf("buzz");
+else
+printf("%d", n);
+if (n != 100)
+printf(" ");
+}
+printf("\n");
+return (0);
+}
