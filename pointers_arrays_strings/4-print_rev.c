36dd00abcfcaa3223d628a3a55536d87bacd0d1a
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-31 09:05:44 +0000

    ex 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_rev - prints a string in reverse
+* @s: strings to reverse.
+*/
+
+void print_rev(char *s)
+{
+	char n;
+	for (; *s != '\0'; s++)
+	{
+		for (n >= '\0'; n <= 0; n--)
+		_putchar(n);
+	}
+	_putchar(*s);
+	_putchar('\n');
+}
