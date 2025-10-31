827b299d4608704dd74adbf38ce4e1d5b4bed995
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-31 19:13:09 +0000

    ex 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strcpy - copy the strings
+* @dest: the dest of the string
+* @src: the source of the string
+* Return : the string
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int c = 0;
+	
+	while (src[c] != '\0')
+	{
+		dest[c] = src[c];
+		c = c + 1;
+	}
+	dest[c] = '\0';
+	return (dest);
+}
