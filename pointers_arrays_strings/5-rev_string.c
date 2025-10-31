27adeed26b8919c8598954cb41ae40acd5bde726
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-31 12:48:21 +0000

    ex 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,13 +8,11 @@
 
 void rev_string(char *s)
 {
-	int cnt = 0;
-	int str, swp;
-
-	while (s[cnt] != '\0')
-	cnt++;
+	int cnt, str, swp;
+	
+	for (cnt = 0; s[cnt] != '\0'; cnt++)
+	for (str = 0; str < cnt / 2; str++)
 	{
-		for (str = 0; str < cnt / 2; str++)
 		swp = s[str];
 		s[str] = s[cnt - 1 - str];
 		s[cnt - 1 - str] = swp;
