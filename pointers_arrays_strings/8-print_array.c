a175a1d1a96009d5bae1dabde094d21bd3e68dec
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-31 18:52:36 +0000

    ex 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,11 +1,25 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
-*
-*
+* print_array - Print the elements
+* @a: var to print
+* @n: num to print
+* Return: void
 */
 
 void print_array(int *a, int n)
 {
-	
+	int c = 0;
+
+    for (c = 0; c < n; c++)
+	{
+		printf("%d", a[c]);
+		if (c != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
 }
