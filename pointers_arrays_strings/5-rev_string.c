2c002ad6488b151b523813df2d280b4f8ac45dde
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-31 10:49:34 +0000

    ex 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,12 +8,12 @@
 
 void rev_string(char *s)
 {
-	int cnt, str, swap;
+	int cnt, str, swp;
 	for (cnt = 0; s[cnt] != '\0'; cnt++)
 	{
-		cnt = *s;
-		*s = str;
-		str = swap;
-		swap = cnt;
+		for (str = 0; str < cnt / 2; str++)
+		swp = s[str];
+		s[str] = s[cnt - 1 - str];
+		s[cnt - 1 - str] = swp;
 	}
 }
