6fd90023576064df66402d438d7f13a5ed1a365d
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-31 14:52:30 +0000

    ex 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -10,10 +10,9 @@ void puts_half(char *str)
 	int i = 0;
 
 	while (str[i] != '\0')
-	{
-		for (str = 0; str < i / 2; str++)
-		_putchar(str[i]);
 		i++;
-	}
+	
+	for (str = 0; str < i / 2; str++)
+	_putchar(str[i]);
 	_putchar('\n');
 }
