bcbf3a192ca9c1519be5f45157d3ed023df0b791
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-31 13:35:55 +0000

    ex 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,7 +7,11 @@
 
 void puts2(char *str)
 {
-	for (; *str != '\0' / 2; str++)
+	int c = 0;
+	while (str[c] != '\0')
+		c++;
+	
+	for (str = 0; *str < c / 2; str++)
 	_putchar(*str);
 	_putchar('\n');
 }
