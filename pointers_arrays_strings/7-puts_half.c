58c946e40743b7436f14f54b81b25d8ce6ec000b
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-31 15:07:52 +0000

    ex 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -2,13 +2,18 @@
 
 /**
 * puts_half - that prints half of a string,
-* @str: string to print.
+* @str: string to print
 */
 
 void puts_half(char *str)
 {
 	char *i = str;
 	int lg = 0;
+
+	while (*(i + lg) != '\0')
+	{
+		lg++;
+	}
 	i = i + (lg + 1) / 2;
 	
 	while (*i != '\0')
