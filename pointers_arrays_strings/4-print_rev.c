29eddaa72301c91cccf7a5398d6ecaf68ae82bb5
B-ama-k <Benjamin.fln74@gmail.com>
2025-10-31 10:26:12 +0000

    ex 5

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -13,7 +13,7 @@ void print_rev(char *s)
 	{}
 		for (i = c - 1; i >= 0; i--)
 		{
-		_putchar(s[i]);
+			_putchar(s[i]);
 		}
 	_putchar('\n');
 }
---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*
+*
+*
+*/
+
+void rev_string(char *s)
+{
+	int cnt, str, swap;
+	for (cnt = 0; s[cnt] != '\0'; cnt++)
+	{
+		cnt = *s;
+		*s = str;
+		str = swap;
+		swap = cnt;
+	}
+}
