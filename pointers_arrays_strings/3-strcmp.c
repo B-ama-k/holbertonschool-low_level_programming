c930bb1d8066e4b068c713ef0bfeaf557000244b
B-ama-k <Benjamin.fln74@gmail.com>
2025-11-02 06:41:17 +0000

    ex 3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strcmp - compare two strings.
+* @s1: first string.
+* @s2: second string.
+* Return: 
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int c = 0;
+	
+	while (s1[c] != '\0' && s2[c] != '\0')
+	{
+		if (s1[c] != s2[c] != '\0')
+		return (s1[c] - s2[c]);
+		c++;
+	}
+}
