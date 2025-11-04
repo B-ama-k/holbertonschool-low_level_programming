ed003262d6af1374588badbfa39c0b65e2edcd1b
B-ama-k <Benjamin.fln74@gmail.com>
2025-11-04 17:54:57 +0000

    ex 7

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+* print_diagsums - prints the sum of two diag 
+* of a square matrix of integers.
+* @a: pointe to a string.
+* @size: the size of the diag.
+*/
+
+void print_diagsums(int *a, int size)
