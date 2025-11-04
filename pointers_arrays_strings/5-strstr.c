202dd537c488eabda529be338ccdefcf74150bb7
B-ama-k <Benjamin.fln74@gmail.com>
2025-11-04 14:07:29 +0000

    ex 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _strstr - locates a substring.
+* @needle: find the first occurrence of the substring.
+* @haystack: find in this string.
+* Return: 
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	if (needle[0] == '\0')
+		return (haystack);
+	
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (needle[j] == '\0')
+			return (haystack + i);	
+	}
+	return (0);
+}
