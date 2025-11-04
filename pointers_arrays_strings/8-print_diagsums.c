8f26931e18ba7bf3682740930a09b0e96f763ec8
B-ama-k <Benjamin.fln74@gmail.com>
2025-11-04 18:00:05 +0000

    ex 8

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
 * print_diagsums - prints the sum of two diag 
 * of a square matrix of integers.
@@ -8,3 +8,13 @@
 */
 
 void print_diagsums(int *a, int size)
+{
+	int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 = sum1 + a[i * size + 1];
+		sum2 = sum2 + a[i * size + (size - 1 - i)];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
