23c8901571c410cee44614e6053975d1ddab63d2
B-ama-k <Benjamin.fln74@gmail.com>
2025-11-07 12:18:25 +0000

    ex 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - print the name
+* @argc: num of argurment.
+* @argv: array of argument.
+* Return: 0.
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
