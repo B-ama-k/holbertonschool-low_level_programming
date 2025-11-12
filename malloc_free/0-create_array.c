ca2dd893e7890641cd0bb7f97dae0ee9dee14003
B-ama-k <Benjamin.fln74@gmail.com>
2025-11-12 09:38:15 +0000

    ex 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i = 0;
 
-	if (size == 0);
+	if (size == 0)
 	{
 		return (NULL);
 	}
@@ -27,7 +27,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	for (; i < size; i++);
+	for (; i < size; i++)
 	{
 		arr[i] = c;
 	}
