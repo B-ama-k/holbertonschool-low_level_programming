0135ef77e40ea745ce1e948a6890aff63a99d1ab
B-ama-k <Benjamin.fln74@gmail.com>
2025-12-05 10:21:33 +0000

    ex 0 + main.h + README.md

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* read_textfile - Reads and print content of a file to stdout.
+* @filename: Name of the file.
+* @letters: Numbers of letters to read and print.
+* Return: The number of letters read and printed,
+*         or 0 on failure.
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t n;
+	int fd;
+	char *buff;
+
+	if (!filename || !letters)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+	if (!buff)
+		return (0);
+
+	n = read(fd, buff, letters);
+	if (n == -1 || write(STDOUT_FILENO, buff, n) != n)
+	{
+		free(buff);
+		return (0);
+	}
+
+	close(fd);
+	free(buff);
+	return (n);
+}
